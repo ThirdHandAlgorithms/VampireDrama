da392f5d0f2c4b1de203f7588241683ca07c8d07
Partouf <partouf@gmail.com>
2019-04-24 23:52:38 +0200

    holy aura effects vampire negatively every 1sec in a tiny radius, effects humans positively

---
+++ Assets/Scripts/Map/AuraEffect.cs
@@ -0,0 +1,10 @@
+﻿namespace VampireDrama
+{
+    using UnityEngine;
+
+    public abstract class AuraEffect : MonoBehaviour
+    {
+        public abstract void Affect(Human obj);
+        public abstract void Affect(VampirePlayer obj);
+    }
+}
