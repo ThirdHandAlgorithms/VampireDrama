ec7901913bfbbfd7de5bdb2dfd720cfaeceb8c6a
Partouf <partouf@gmail.com>
2019-04-24 23:54:55 +0200

    bugfix aura

--- Assets/Scripts/Map/AuraEffect.cs
+++ Assets/Scripts/Map/AuraEffect.cs
@@ -2,7 +2,7 @@
 {
     using UnityEngine;
 
-    public abstract class AuraEffect : MonoBehaviour
+    public abstract class AuraEffect
     {
         public abstract void Affect(Human obj);
         public abstract void Affect(VampirePlayer obj);
