32023bf77add60ffa7c51fa55e4dd98bcd2b4829
braks <hawnsbrix@users.noreply.github.com>
2019-04-25 20:08:40 +0200

    Update ChunkPresets.cs

--- Assets/Scripts/Map/ChunkPresets.cs
+++ Assets/Scripts/Map/ChunkPresets.cs
@@ -70,6 +70,11 @@
                         current = new Bridge();
                         current.Dir = ConstructHVDirection.Vertical;
                     }
+					else if (ch == '@')
+					{
+						current = new Bridge();
+						current.Dir = ConstructHVDirection.Horizontal;
+					}
                     else if (ch == 'T')
                     {
                         current = new Tavern();
@@ -156,7 +161,7 @@
             var preset = new string[6];
             preset[5] = " ^|   ";
             preset[4] = " ^| X=";
-            preset[3] = " ^|   ";
+            preset[3] = " @    ";
             preset[2] = " ^| ==";
             preset[1] = " ^|   ";
             preset[0] = " ^| ==";
