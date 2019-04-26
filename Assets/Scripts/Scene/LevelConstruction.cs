9380b803ddb64d6b48b4ac862398eda81c605221
Partouf <partouf@gmail.com>
2019-04-26 12:50:41 +0200

    separate levelconstruction somewhat + lots of refactoring

---
+++ Assets/Scripts/Scene/LevelConstruction.cs
@@ -0,0 +1,301 @@
+﻿namespace VampireDrama
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+
+    public class LevelConstruction : MonoBehaviour
+    {
+        public GameObject Player;
+        public GameObject[] RoadCrossing;
+        public GameObject[] RoadV;
+        public GameObject[] RoadH;
+        public GameObject[] BuildingV;
+        public GameObject[] BuildingH;
+        public GameObject[] WaterV;
+        public GameObject[] WaterH;
+        public GameObject[] BridgeV;
+        public GameObject[] BridgeH;
+        public GameObject[] Trash;
+        public GameObject BorderN;
+        public GameObject BorderE;
+        public GameObject BorderS;
+        public GameObject BorderW;
+        public GameObject Exit;
+        public GameObject StreetLight;
+        public GameObject[] TavernH;
+        public GameObject[] MausoleumH;
+        public GameObject[] ChurchH;
+        public GameObject[] Bloodstain;
+        public GameObject[] BloodPrefabs;
+
+        protected List<GameObject> humans;
+        protected List<GameObject> allObjects;
+        protected GameObject exitInstance;
+
+        protected float tileSize = 1.0f;
+        public Map currentMap;
+        protected ConstructionChunk fullMap;
+        protected int lineCount;
+        protected int startAndExit;
+        protected string startOfRandomState;
+
+        public LevelConstruction()
+        {
+        }
+
+        public virtual void InitScene(int level)
+        {
+            allObjects = new List<GameObject>();
+            humans = new List<GameObject>();
+
+            var config = MapConfiguration.getInstance();
+            config.Height = level * 12;
+            lineCount = config.Height;
+            startAndExit = 6;
+
+            currentMap = new Map();
+
+            //int seed = 1;
+            //Random.InitState(seed);
+            startOfRandomState = JsonUtility.ToJson(Random.state);
+
+            currentMap.GenerateMapWithChunks();
+            MapTest mapTest = new MapTest(currentMap);
+            while (!mapTest.IsTraversable())
+            {
+                //seed++;
+                //Random.InitState(seed);
+                startOfRandomState = JsonUtility.ToJson(Random.state);
+                currentMap.GenerateMapWithChunks();
+                mapTest = new MapTest(currentMap);
+            }
+
+            Debug.Log(startOfRandomState);
+
+            fullMap = currentMap.GetFullmap();
+
+            for (var lineIdx = 0; lineIdx < config.Height; lineIdx++)
+            {
+                RenderLine(lineIdx);
+            }
+
+            var amountOfHumans = (lineCount / 6) + ((level - 1) * 2);
+            for (var idx = 0; idx < amountOfHumans; idx++)
+            {
+                AddHuman();
+            }
+        }
+
+        private GameObject GetRandomHumanTemplate()
+        {
+            var nextPick = (int)(Random.value * BloodPrefabs.Length);
+            return BloodPrefabs[nextPick];
+        }
+
+        private bool IsOccupiedByOtherHumans(int x, int y)
+        {
+            foreach (var food in humans)
+            {
+                if ((food.transform.position.x == x) && (food.transform.position.y == y))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsAreaOkForHuman(int x, int y)
+        {
+            if ((Player.transform.position.x == x) && (Player.transform.position.y == y))
+            {
+                return false;
+            }
+
+            var construct = fullMap[y][x];
+            bool passable = construct.Passable && (construct.Id == ConstructionType.Road);
+
+            return passable && !IsOccupiedByOtherHumans(x, y);
+        }
+
+        private Vector3 GetRandomV3()
+        {
+            var config = MapConfiguration.getInstance();
+            return new Vector3((int)(Random.value * config.Width), (int)(Random.value * config.Height), 0);
+        }
+
+        private void AddHuman()
+        {
+            Vector3 position;
+            position = GetRandomV3();
+            while (!IsAreaOkForHuman((int)(position.x), (int)(position.y)))
+            {
+                position = GetRandomV3();
+            }
+
+            var human = Instantiate(GetRandomHumanTemplate(), position, Quaternion.identity) as GameObject;
+            humans.Add(human);
+        }
+
+        private GameObject GetTemplateGameObjectForConstruct(Construct construct)
+        {
+            if (construct.Id == ConstructionType.Road && construct.Dir == ConstructHVDirection.Vertical)
+            {
+                return RoadV[0];
+            }
+            else if (construct.Id == ConstructionType.Road && construct.Dir == ConstructHVDirection.Horizontal)
+            {
+                return RoadH[0];
+            }
+            else if (construct.Id == ConstructionType.Road && construct.Dir == ConstructHVDirection.None)
+            {
+                return RoadCrossing[0];
+            }
+            else if (construct.Id == ConstructionType.Building && construct.Dir == ConstructHVDirection.Vertical)
+            {
+                return BuildingV[0];
+            }
+            else if (construct.Id == ConstructionType.Building && construct.Dir == ConstructHVDirection.Horizontal)
+            {
+                return BuildingH[0];
+            }
+            else if (construct.Id == ConstructionType.Water && construct.Dir == ConstructHVDirection.Vertical)
+            {
+                return WaterV[0];
+            }
+            else if (construct.Id == ConstructionType.Water && construct.Dir == ConstructHVDirection.Horizontal)
+            {
+                return WaterH[0];
+            }
+            else if (construct.Id == ConstructionType.Bridge && construct.Dir == ConstructHVDirection.Vertical)
+            {
+                return BridgeV[0];
+            }
+            else if (construct.Id == ConstructionType.Bridge && construct.Dir == ConstructHVDirection.Horizontal)
+            {
+                return BridgeH[0];
+            }
+            else if (construct.Id == ConstructionType.Dumpster)
+            {
+                return Trash[0];
+            }
+            else if (construct.Id == ConstructionType.Tavern)
+            {
+                return TavernH[0];
+            }
+            else if (construct.Id == ConstructionType.Mausoleum)
+            {
+                return MausoleumH[0];
+            }
+            else if (construct.Id == ConstructionType.Church)
+            {
+                return ChurchH[0];
+            }
+
+            return null;
+        }
+
+        private GameObject AddInstance(GameObject template, float x, float y)
+        {
+            GameObject borderObj;
+            borderObj = Instantiate(template, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+            allObjects.Add(borderObj);
+
+            return borderObj;
+        }
+
+        private GameObject AddStreetlight(float x, float y)
+        {
+            GameObject borderObj;
+            borderObj = Instantiate(StreetLight, new Vector3(x, y, -.5f), Quaternion.identity) as GameObject;
+            allObjects.Add(borderObj);
+
+            return borderObj;
+        }
+
+        private void RenderBorders(int lineIdx, ConstructionLine line)
+        {
+            AddInstance(BorderE, line.Count * tileSize, lineIdx * tileSize);
+            AddInstance(BorderW, -1 * tileSize, lineIdx * tileSize);
+
+            if (lineIdx == 0)
+            {
+                for (var x = 0; x < line.Count; x++)
+                {
+                    AddInstance(BorderS, x * tileSize, -1 * tileSize);
+                }
+            }
+
+            if (lineIdx == lineCount - 1)
+            {
+                for (var x = 0; x < line.Count; x++)
+                {
+                    if (x != startAndExit)
+                    {
+                        AddInstance(BorderN, x * tileSize, lineCount * tileSize);
+                    }
+                    else
+                    {
+                        exitInstance = AddInstance(Exit, x * tileSize, lineCount * tileSize);
+                    }
+                }
+            }
+        }
+
+        private void RenderLine(int lineIdx)
+        {
+            var line = fullMap[lineIdx];
+
+            if (lineIdx == 0)
+            {
+                InitializePlayerPosition(line);
+            }
+
+            RenderBorders(lineIdx, line);
+
+            var x = 0;
+            foreach (var construct in line)
+            {
+                GameObject templateGameObject = GetTemplateGameObjectForConstruct(construct);
+
+                if (templateGameObject != null)
+                {
+                    AddInstance(templateGameObject, x * tileSize, lineIdx * tileSize);
+
+                    if (construct.HasLightSource)
+                    {
+                        AddStreetlight(x * tileSize, lineIdx * tileSize);
+                    }
+                }
+
+                x++;
+            }
+        }
+
+        private void InitializePlayerPosition(ConstructionLine line)
+        {
+            if (Player == null) return;
+
+            Player.transform.position = new Vector3(startAndExit * tileSize, 0f, 0f);
+        }
+
+
+        protected void ClearScene()
+        {
+            foreach (var obj in humans)
+            {
+                Destroy(obj);
+            }
+            humans.Clear();
+
+            exitInstance = null;
+
+            foreach (var obj in allObjects)
+            {
+                Destroy(obj);
+            }
+            allObjects.Clear();
+        }
+
+    }
+}
--- Assets/Scripts/Scene/SceneManager.cs
+++ Assets/Scripts/Scene/SceneManager.cs
@@ -1,112 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using VampireDrama;
 
-public class SceneManager : MonoBehaviour {
-    public GameObject Player;
-    public GameObject[] RoadCrossing;
-    public GameObject[] RoadV;
-    public GameObject[] RoadH;
-    public GameObject[] BuildingV;
-    public GameObject[] BuildingH;
-    public GameObject[] WaterV;
-    public GameObject[] WaterH;
-    public GameObject[] BridgeV;
-    public GameObject[] BridgeH;
-    public GameObject[] Trash;
-    public GameObject BorderN;
-    public GameObject BorderE;
-    public GameObject BorderS;
-    public GameObject BorderW;
-    public GameObject Exit;
-    public GameObject StreetLight;
-    public GameObject[] TavernH;
-    public GameObject[] MausoleumH;
-    public GameObject[] ChurchH;
-    public GameObject[] Bloodstain;
+public class SceneManager : LevelConstruction
+{
     public Text XPText;
     public Text BloodfillText;
     public Text TimeOfDayText;
 
-    public GameObject[] BloodPrefabs;
-    private List<GameObject> cattle;
-
-    private List<GameObject> allObjects;
-    private GameObject exitInstance;
-
-    private float tileSize = 1.0f;
-    public Map currentMap;
-    private ConstructionChunk fullMap;
-    private int currentLine;
-    private int lineCount;
-    private int startAndExit;
-    private string startOfRandomState;
-
     private float startTimeOfDay;
     private float lastSunAuraTime;
-
-    private void ClearScene()
-    {
-        foreach (var obj in cattle)
-        {
-            Destroy(obj);
-        }
-        cattle.Clear();
-
-        foreach (var obj in allObjects)
-        {
-            Destroy(obj);
-        }
-        allObjects.Clear();
-    }
-
-    public void InitScene(int level)
-	{
-        allObjects = new List<GameObject>();
-        cattle = new List<GameObject>();
-
-        var config = MapConfiguration.getInstance();
-        config.Height = level * 12;
-        lineCount = config.Height;
-        currentLine = 0;
-        startAndExit = 6;
-
-        currentMap = new Map();
-
-        //int seed = 1;
-        //Random.InitState(seed);
-        startOfRandomState = JsonUtility.ToJson(Random.state);
-
-        currentMap.GenerateMapWithChunks();
-        MapTest mapTest = new MapTest(currentMap);
-        while (!mapTest.IsTraversable())
-        {
-            //seed++;
-            //Random.InitState(seed);
-            startOfRandomState = JsonUtility.ToJson(Random.state);
-            currentMap.GenerateMapWithChunks();
-            mapTest = new MapTest(currentMap);
-        }
-
-        Debug.Log(startOfRandomState);
-
-        fullMap = currentMap.GetFullmap();
-
-        for (var lineIdx = 0; lineIdx < config.Height; lineIdx++)
-        {
-            RenderLine(lineIdx);
-        }
-
-        var amountOfCattle = (lineCount / 6) + ((level - 1) * 2);
-        for (var idx = 0; idx < amountOfCattle; idx++)
-        {
-            AddBlood();
-        }
-
-        startTimeOfDay = Time.time;
-    }
 
     public void Stop()
     {
@@ -115,196 +18,32 @@ public class SceneManager : MonoBehaviour {
 
         ClearScene();
     }
-
-    private GameObject GetNextBloodTemplate()
+        
+    public override void InitScene(int level)
     {
-        var nextPick = (int)(Random.value * BloodPrefabs.Length);
-        return BloodPrefabs[nextPick];
+        base.InitScene(level);
+
+        startTimeOfDay = Time.time;
     }
 
-    private bool IsOccupiedByOtherFood(int x, int y)
+    // Update is called once per frame
+    public void Update ()
     {
-        foreach (var food in cattle)
+        var cameras = Camera.allCameras;
+        if (cameras.Length > 0)
         {
-            if ((food.transform.position.x == x) && (food.transform.position.y == y))
-            {
-                return true;
-            }
-        }
+            cameras[0].transform.position = new Vector3(6, Player.transform.position.y, -15f);
 
-        return false;
-    }
+            DisplayPlayerStats();
 
-    private bool IsAreaOkForHuman(int x, int y)
-    {
-        if ((Player.transform.position.x == x) && (Player.transform.position.y == y))
-        {
-            return false;
-        }
+            int hour, minute;
+            getTimeOfDay(out hour, out minute);
+            DisplayTimeOfDay(hour, minute);
 
-        var construct = fullMap[y][x];
-        bool passable = construct.Passable && (construct.Id == ConstructionType.Road);
-
-        return passable && !IsOccupiedByOtherFood(x, y);
-    }
-
-    private Vector3 GetRandomV3()
-    {
-        var config = MapConfiguration.getInstance();
-        return new Vector3((int)(Random.value * config.Width), (int)(Random.value * config.Height), 0);
-    }
-
-    private void AddBlood()
-    {
-        Vector3 position;
-        position = GetRandomV3();
-        while (!IsAreaOkForHuman((int)(position.x), (int)(position.y)))
-        {
-            position = GetRandomV3();
-        }
-
-        var victim = Instantiate(GetNextBloodTemplate(), position, Quaternion.identity) as GameObject;
-        cattle.Add(victim);
-    }
-
-    private GameObject GetTemplateGameObjectForConstruct(Construct construct)
-    {
-        if (construct.Id == ConstructionType.Road && construct.Dir == ConstructHVDirection.Vertical)
-        {
-            return RoadV[0];
-        }
-        else if (construct.Id == ConstructionType.Road && construct.Dir == ConstructHVDirection.Horizontal)
-        {
-            return RoadH[0];
-        }
-        else if (construct.Id == ConstructionType.Road && construct.Dir == ConstructHVDirection.None)
-        {
-            return RoadCrossing[0];
-        }
-        else if (construct.Id == ConstructionType.Building && construct.Dir == ConstructHVDirection.Vertical)
-        {
-            return BuildingV[0];
-        }
-        else if (construct.Id == ConstructionType.Building && construct.Dir == ConstructHVDirection.Horizontal)
-        {
-            return BuildingH[0];
-        }
-        else if (construct.Id == ConstructionType.Water && construct.Dir == ConstructHVDirection.Vertical)
-        {
-            return WaterV[0];
-        }
-        else if (construct.Id == ConstructionType.Water && construct.Dir == ConstructHVDirection.Horizontal)
-        {
-            return WaterH[0];
-        }
-        else if (construct.Id == ConstructionType.Bridge && construct.Dir == ConstructHVDirection.Vertical)
-        {
-            return BridgeV[0];
-        }
-        else if (construct.Id == ConstructionType.Bridge && construct.Dir == ConstructHVDirection.Horizontal)
-        {
-            return BridgeH[0];
-        }
-        else if (construct.Id == ConstructionType.Dumpster)
-        {
-            return Trash[0];
-        }
-        else if (construct.Id == ConstructionType.Tavern)
-        {
-            return TavernH[0];
-        }
-        else if (construct.Id == ConstructionType.Mausoleum)
-        {
-            return MausoleumH[0];
-        }
-        else if (construct.Id == ConstructionType.Church)
-        {
-            return ChurchH[0];
-        }
-
-        return null;
-    }
-
-    private void RenderBorders(int lineIdx, ConstructionLine line)
-    {
-        GameObject borderObj;
-        borderObj = Instantiate(BorderE, new Vector3(line.Count * tileSize, lineIdx * tileSize, 0f), Quaternion.identity) as GameObject;
-        allObjects.Add(borderObj);
-
-        borderObj = Instantiate(BorderW, new Vector3(-1 * tileSize, lineIdx * tileSize, 0f), Quaternion.identity) as GameObject;
-        allObjects.Add(borderObj);
-
-        if (lineIdx == 0)
-        {
-            for (var x = 0; x < line.Count; x++)
-            {
-                borderObj = Instantiate(BorderS, new Vector3(x * tileSize, -1 * tileSize, 0f), Quaternion.identity) as GameObject;
-                allObjects.Add(borderObj);
-            }
-        }
-
-        if (lineIdx == lineCount - 1)
-        {
-            for (var x = 0; x < line.Count; x++)
-            {
-                if (x != startAndExit)
-                {
-                    borderObj = Instantiate(BorderN, new Vector3(x * tileSize, lineCount * tileSize, 0f), Quaternion.identity) as GameObject;
-                    allObjects.Add(borderObj);
-                }
-                else
-                {
-                    exitInstance = Instantiate(Exit, new Vector3(x * tileSize, lineCount * tileSize, 0f), Quaternion.identity) as GameObject;
-                    allObjects.Add(exitInstance);
-                }
-            }
+            HandleTimeOfDay(hour);
         }
     }
 
-    private void RenderLine(int lineIdx)
-    {
-        var line = fullMap[lineIdx];
-
-        if (lineIdx == 0)
-        {
-            InitializePlayerPosition(line);
-        }
-
-        RenderBorders(lineIdx, line);
-
-        var x = 0;
-        foreach (var construct in line)
-        {
-            GameObject templateGameObject = GetTemplateGameObjectForConstruct(construct);
-
-            if (templateGameObject != null)
-            {
-                var tileObj = Instantiate(templateGameObject, new Vector3(x * tileSize, lineIdx * tileSize, 0f), Quaternion.identity) as GameObject;
-                allObjects.Add(tileObj);
-
-                if (construct.HasLightSource)
-                {
-                    var lightObj = Instantiate(StreetLight, new Vector3(x * tileSize, lineIdx * tileSize, -.5f), Quaternion.identity) as GameObject;
-                    allObjects.Add(lightObj);
-                }
-            }
-
-            x++;
-        }
-    }
-
-    private void InitializePlayerPosition(ConstructionLine line)
-    {
-        if (Player == null) return;
-
-        Player.transform.position = new Vector3(startAndExit * tileSize, 0f, 0f);
-    }
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
     private void getTimeOfDay(out int Hour, out int Minute)
     {
         // 1s realtime is 1minute gametime
@@ -317,42 +56,38 @@ public class SceneManager : MonoBehaviour {
         Hour = Hour % 24;
     }
 
-    // Update is called once per frame
-    void Update () {
-        var cameras = Camera.allCameras;
-        if (cameras.Length > 0)
+    private void HandleTimeOfDay(int hour)
+    {
+        if (hour >= 6 && hour < 22)
         {
-            currentLine = (int)(Player.transform.position.y);
-
-            cameras[0].transform.position = new Vector3(6, Player.transform.position.y, -15f);
-
-            var player = Player.GetComponent<VampirePlayer>();
-            XPText.text = "XP: " + player.Experience.ToString();
-            BloodfillText.text = "Blood: " + player.Bloodfill.ToString();
-
-            int hour, minute;
-            getTimeOfDay(out hour, out minute);
-            TimeOfDayText.text = "Time: " + hour.ToString() + ":" + minute.ToString("00");
-
-            if (hour >= 6 && hour < 22)
+            // between 6 and 22 you're going to burn
+            if (Time.time - lastSunAuraTime >= 1f)
             {
-                // between 6 and 22 you're going to burn
-                if (Time.time - lastSunAuraTime >= 1f)
-                {
-                    lastSunAuraTime = Time.time;
+                lastSunAuraTime = Time.time;
 
-                    var sun = new SunAuraEffect();
-                    sun.Strength = hour - 5;    // so at 6:00, you lose 1 blood every second, at 7:00 2 blood every second...
-                    ApplyAuraEffectEverywhere(sun);
-                }
+                var sun = new SunAuraEffect();
+                sun.Strength = hour - 5;    // so at 6:00, you lose 1 blood every second, at 7:00 2 blood every second...
+                ApplyAuraEffectEverywhere(sun);
             }
         }
+    }
+
+    private void DisplayTimeOfDay(int hour, int minute)
+    {
+        TimeOfDayText.text = "Time: " + hour.ToString() + ":" + minute.ToString("00");
+    }
+
+    private void DisplayPlayerStats()
+    {
+        var player = Player.GetComponent<VampirePlayer>();
+        XPText.text = "XP: " + player.Experience.ToString();
+        BloodfillText.text = "Blood: " + player.Bloodfill.ToString();
     }
 
     public void Kill(Human target, GameObject obj)
     {
         Vector3 killspot = obj.transform.position;
-        cattle.Remove(obj);
+        humans.Remove(obj);
         Destroy(obj);
 
 
@@ -373,7 +108,7 @@ public class SceneManager : MonoBehaviour {
 
     public void ApplyAuraEffect(int posx, int posy, AuraEffect effect)
     {
-        foreach (var obj in cattle)
+        foreach (var obj in humans)
         {
             if (obj.transform.position.x == posx && obj.transform.position.y == posy)
             {
@@ -388,9 +123,10 @@ public class SceneManager : MonoBehaviour {
             if (vamp != null) effect.Affect(vamp);
         }
     }
+
     public void ApplyAuraEffectEverywhere(AuraEffect effect)
     {
-        foreach (var obj in cattle)
+        foreach (var obj in humans)
         {
             var human = obj.GetComponent<Human>();
             if (human != null) effect.Affect(human);
