0ec3baae24a2177862f5a56f4bea3e87d4b2ceea
braks <hawnsbrix@users.noreply.github.com>
2019-04-27 20:37:29 +0200

    Update UiRollover.cs

--- Assets/Scripts/Scene/UiRollover.cs
+++ Assets/Scripts/Scene/UiRollover.cs
@@ -5,40 +5,38 @@ using UnityEngine;
 
 public class UiRollover : MonoBehaviour
 {
-    public void RolloverPlayerStats()
+    public Text transitionTxt;
+    public float animationTime = 1.5f; 
+
+    public float desiredNr, initialNr, currentNr;
+
+    public void setNr(float value)
     {
-        public Text transitionTxt;
-        public float animationTime = 1.5f; 
-
-        public float desiredNr, initialNr, currentNr;
-
-        public void setNr(float value)
+        initialNr = currentNr;
+        desiredNr = value;
+    } 
+    public void addToNr(float value) 
+    {
+        initialNr = currentNr;
+        desiredNr += value;
+    }
+    public void UpdateNr()
+    {
+        if(currentNr !=desiredNr)
         {
-            initialNr = currentNr;
-            desiredNr = value;
-        } 
-        public void addToNr(float value) 
-        {
-            initialNr = currentNr;
-            desiredNr += value;
+            if(initialNr < desiredNr)
+            {
+                currentNr += (animationTime * Time.deltaTime) * (desiredNr - initialNr);
+            }
         }
-        public void UpdateNr()
+        else
         {
-            if(currentNr !=desiredNr)
-            {
-                if(initialNr < desiredNr)
-                {
-                    currentNr += (animationTime * Time.deltaTime) * (desiredNr - initialNr);
-                }
-            }
-            else
-            {
-                currentNr -= (animationTime * Time.deltaTime) * (initialNr - desiredNr);
-                if(currentNr <= desiredNr)
-                  currentNr = desiredNr;  
-            }
+            currentNr -= (animationTime * Time.deltaTime) * (initialNr - desiredNr);
+            if(currentNr <= desiredNr)
+               currentNr = desiredNr;  
+        }
              
-            transitionTxt.text = currentNr.ToString();
-        }
+        transitionTxt.text = currentNr.ToString();
+        
     }
 }
